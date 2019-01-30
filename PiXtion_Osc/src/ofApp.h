93c8e459e0222441f2fab6ccb2cf6cccb8be781a
Nick Fox-Gieg <nick@fox-gieg.com>
2019-01-30 13:14:05 -0500

    Update ofApp.h

--- PiXtion_Osc/src/ofApp.h
+++ PiXtion_Osc/src/ofApp.h
@@ -32,6 +32,7 @@ class ofApp : public ofBaseApp {
         string xmlStructure;
         string message;
 
+		ofFile file;
         string compname;
 		string host; // hostname;
 		int port; // default 7110;
