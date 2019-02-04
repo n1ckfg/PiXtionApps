8c2feb9d608df9f2b50265b1c5c378d1b3c58a77
Nick Fox-Gieg <nick@fox-gieg.com>
2019-02-04 11:47:58 -0500

    Update ofApp.cpp

--- PiXtion_Contour/src/ofApp.cpp
+++ PiXtion_Contour/src/ofApp.cpp
@@ -80,7 +80,10 @@ void ofApp::update() {
 
         colorImage.setFromPixels(oniGrabber.rgbSource.currentPixels->getPixels(), settings.width, settings.height);
         colorImage.mirror(false, mirror);
-        toOf(colorImage.getCvImage(), color.getPixelsRef());
+        //toOf(colorImage.getCvImage(), color.getPixelsRef());
+
+        color.setFromPixels(oniGrabber.rgbSource.currentPixels->getPixels(), settings.width, settings.height);
+        color.mirror(false, mirror);
 	}
 }
 
