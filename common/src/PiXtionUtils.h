16caacdc810f6c5807c6c822a08cb382fd09fa16
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-10 11:25:30 -0400

    Update PiXtionUtils.h

--- common/src/PiXtionUtils.h
+++ common/src/PiXtionUtils.h
@@ -4,12 +4,31 @@
 
 namespace PiXtionUtils {
 
-	public:
-		void imageToBuffer(ofImage _img, ofBuffer& _buffer, int _quality);
-		void pixelsToBuffer(ofPixels _pix, ofBuffer& _buffer, int _quality);
-		void fboToBuffer(ofFbo _fbo, ofBuffer& _buffer, int _quality);
+	//void imageToBuffer(ofImage _img, ofBuffer& _buffer, int _quality);
+	void imageToBuffer(ofImage _img, ofBuffer& _buffer, int _quality) {
+	    switch(_quality) {
+	        case 5:
+	            ofSaveImage(_img, _buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_BEST);
+	            break;
+	        case 4:
+	            ofSaveImage(_img, _buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_HIGH);
+	            break;
+	        case 3:
+	            ofSaveImage(_img, _buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_MEDIUM);
+	            break;
+	        case 2:
+	            ofSaveImage(_img, _buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_LOW);
+	            break;
+	        case 1:
+	            ofSaveImage(_img, _buffer, OF_IMAGE_FORMAT_JPEG, OF_IMAGE_QUALITY_WORST);
+	            break;
+	    }
+	}
 
-		string uniqueId(int len);
-		float rawDepthToMeters(int depthValue);
+	void pixelsToBuffer(ofPixels _pix, ofBuffer& _buffer, int _quality);
+	void fboToBuffer(ofFbo _fbo, ofBuffer& _buffer, int _quality);
+
+	string uniqueId(int len);
+	float rawDepthToMeters(int depthValue);
 
 }
