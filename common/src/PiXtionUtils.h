988d344912361e890d1d338deebf26f3854aa972
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-10 11:24:18 -0400

    Update PiXtionUtils.h

--- common/src/PiXtionUtils.h
+++ common/src/PiXtionUtils.h
@@ -4,11 +4,12 @@
 
 namespace PiXtionUtils {
 
-	void imageToBuffer(ofImage _img, ofBuffer& _buffer, int _quality);
-	void pixelsToBuffer(ofPixels _pix, ofBuffer& _buffer, int _quality);
-	void fboToBuffer(ofFbo _fbo, ofBuffer& _buffer, int _quality);
+	public:
+		void imageToBuffer(ofImage _img, ofBuffer& _buffer, int _quality);
+		void pixelsToBuffer(ofPixels _pix, ofBuffer& _buffer, int _quality);
+		void fboToBuffer(ofFbo _fbo, ofBuffer& _buffer, int _quality);
 
-	string uniqueId(int len);
-	float rawDepthToMeters(int depthValue);
+		string uniqueId(int len);
+		float rawDepthToMeters(int depthValue);
 
 }
