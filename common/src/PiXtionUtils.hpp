129613699eade67051f4ec840c7ed4fa49a2f929
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-11 12:17:25 -0400

    Update PiXtionUtils.hpp

--- common/src/PiXtionUtils.hpp
+++ common/src/PiXtionUtils.hpp
@@ -24,7 +24,7 @@ namespace PiXtionUtils {
     }
 
     void floatsToBuffer(float floats[], ofBuffer& buffer) {
-        unsigned char * chars = reinterpret_cast<unsigned char *>(floats);
+        char const * chars = reinterpret_cast<char const *>(floats);
         std::string charString(chars, chars + sizeof floats);
         buffer.set(charString); 
     }
