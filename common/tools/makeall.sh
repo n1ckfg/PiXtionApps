a0ef28bf43e49cacabdcd2e94d4a230400c93036
Nick Fox-Gieg <nick@fox-gieg.com>
2019-09-10 23:13:03 -0400

    Update makeall.sh

--- common/tools/makeall.sh
+++ common/tools/makeall.sh
@@ -1,11 +1,11 @@
 git pull
-cd ../PiXtion_Basic
+cd ../../PiXtion_Basic
 make
-cd ../PiXtion_Calibration
+cd ../../PiXtion_Calibration
 make
-cd ../PiXtion_Contour
+cd ../../PiXtion_Contour
 make
-cd ../PiXtion_Osc
+cd ../../PiXtion_Osc
 make
-cd ../PiXtion_Scanline
+cd ../../PiXtion_Scanline
 make
